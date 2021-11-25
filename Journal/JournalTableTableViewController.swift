35ef417bb3181b55be8ae91d101331b966580db3
David Svensson <davidsvson@gmail.com>
2021-11-25 09:54:31 +0100

    Tableview working

---
+++ Journal/JournalTableTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  JournalTableTableViewController.swift
+//  Journal
+//
+//  Created by David Svensson on 2021-11-25.
+//
+
+import UIKit
+
+class JournalTableTableViewController: UITableViewController {
+
+    let cellIdentity = "JournalEntryCell"
+    
+    var tableData = ["David", "Susan", "Lois", "Laura" ]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return tableData.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentity, for: indexPath)
+
+        cell.textLabel?.text = tableData[indexPath.row]
+
+        return cell
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
